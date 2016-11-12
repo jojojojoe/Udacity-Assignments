247db7ddbf22ea7071a6449d1ef1452420fc6747
mogen2014 <xiaojun0lee@gmail.com>
2016-11-12 23:06:12 +0800

    async script

--- udacity-portfolio/views/js/main.js
+++ udacity-portfolio/views/js/main.js
@@ -449,10 +449,11 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    var container = document.querySelectorAll(".randomPizzaContainer");
+    var dx = determineDx(container[0], size);
+    var newwidth = (container[0].offsetWidth + dx) + 'px';
+    for (var i = 0; i < container.length; i++) {
+      container[i].style.width = newwidth;
     }
   }
 
