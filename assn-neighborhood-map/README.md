5967a595492e3f7b28236a3d922b3107602e003b
mogen2014 <xiaojun0lee@gmail.com>
2016-11-27 14:24:58 +0800

    a few bugs fixed after first map assignment reviewed

--- assn-neighborhood-map/README.md
+++ assn-neighborhood-map/README.md
@@ -2,6 +2,14 @@ I have being living in this city calld 'Chongqing' for about 4 years, Here is so
 
 ##Usage
 
+### About this app
+1. The index.html file is html file which structured the web page. To run this app you can open that file directly or host it on your localhost, to run an localhost serve I choosed [browsersync](https://www.browsersync.io/) for test convenience.
+2. The map comes from [Google Maps API](https://developers.google.com/maps/).
+3. The map infowindow data comes from [wikipedia API](https://www.mediawiki.org/w/api.php), include the image and sentences.
+4. [Bootstrap](http://getbootstrap.com/) is used to make the page responsive.
+3. I choosed a MVVM style library called [Knockout](http://knockoutjs.com/) to separate my view and data. It has a lot features that help me a lot, such as updating the view automatically and bingding data, it has utilities like filtering a list.
+
+### About the web page
 1. Open the web [page](https://mogen2014.github.io/Udacity-course/assn-neighborhood-map/) you will find a few markers in a Google map.
 2. You can click on the markers to get more infomation on that location, will show a image and a few sentences, all that window showing comes from Wikipedia page.
 3. On the left side there is a location list and an input box that you can input some text to narrow down the list. You can click on the location on that list, it will trigger the selected marker to show detail infomation window.
--- assn-neighborhood-map/js/app.js
+++ assn-neighborhood-map/js/app.js
@@ -1,52 +1,45 @@
-var Model = [{
-    "name": "Jiefangbei CBD",
-    "location": {
-        "lat": 29.557171,
-        "lng": 106.577060
-    },
-    "selected": false
+var model = [{
+    'name': 'Jiefangbei CBD',
+    'location': {
+        'lat': 29.557171,
+        'lng': 106.577060
+    }
 }, {
-    "name": "Ciqikou, Chongqing",
-    "location": {
-        "lat": 29.578068,
-        "lng": 106.450593
-    },
-    "selected": false
+    'name': 'Ciqikou, Chongqing',
+    'location': {
+        'lat': 29.578068,
+        'lng': 106.450593
+    }
 }, {
-    "name": "Chaotianmen Bridge",
-    "location": {
-        "lat": 29.585523,
-        "lng": 106.578911
-    },
-    "selected": false
+    'name': 'Chaotianmen Bridge',
+    'location': {
+        'lat': 29.585523,
+        'lng': 106.578911
+    }
 }, {
-    "name": "Three Gorges Museum",
-    "location": {
-        "lat": 29.562055,
-        "lng": 106.550427
-    },
-    "selected": false
+    'name': 'Three Gorges Museum',
+    'location': {
+        'lat': 29.562055,
+        'lng': 106.550427
+    }
 }, {
-    "name": "Mount Jinfo",
-    "location": {
-        "lat": 29.019637,
-        "lng": 107.174434
-    },
-    "selected": false
+    'name': 'Mount Jinfo',
+    'location': {
+        'lat': 29.019637,
+        'lng': 107.174434
+    }
 }, {
-    "name": "Chongqing",
-    "location": {
-        "lat": 29.565943,
-        "lng": 106.547296
-    },
-    "selected": false
+    'name': 'Chongqing',
+    'location': {
+        'lat': 29.565943,
+        'lng': 106.547296
+    }
 }, {
-    "name": "Dazu Rock Carvings",
-    "location": {
-        "lat": 29.754561,
-        "lng": 105.802660
-    },
-    "selected": false
+    'name': 'Dazu Rock Carvings',
+    'location': {
+        'lat': 29.754561,
+        'lng': 105.802660
+    }
 }, ];
 
 // make markers and infowindow global, so viewmodel can access
@@ -56,14 +49,15 @@ var infowindow;
 // knockout view model
 function ViewModel() {
     var self = this;
-    self.lists = ko.observableArray(Model);
-    self.inputText = ko.observable("");
-    self.selectedName = ko.observable("");
-    self.disableListBtn = ko.observable(true);
+    self.lists = ko.observableArray(model);
+    self.inputText = ko.observable('');
+    self.selectedName = ko.observable('');
+    self.enableListBtn = ko.observable(true);
     self.toggleState = ko.observable(true);
     self.toggle = function(elem) {
         self.toggleState(!self.toggleState());
-        $("#listContainer").slideToggle();
+        $('#listContainer').slideToggle();
+        console.log('toggle');
     };
     // if one item in the lists is selected, simulate the marker is clicked too.
     self.selector = function(e) {
@@ -77,7 +71,6 @@ function ViewModel() {
             markers[key].setIcon(null);
             infowindow.close();
         }
-
         var filter = self.inputText().toLowerCase();
         if (!filter) {
             // if not filter, close info window and reset all markers again 
@@ -101,17 +94,21 @@ function ViewModel() {
         }
     });
 
-    // listen to the window innerWidth changing, if it is wider than 480px, disable the 
-    // listview toggle button, and make sure the list view showing
-    // even if rotate the screen
-    $(window).resize(function() {
-        self.disableListBtn(window.innerWidth > 480);
-        if (window.innerWidth > 480) {
-            self.toggleState(true);
-            $("#listContainer").slideDown();
-        }
+    // listen to the window innerWidth changing or browser resizing,
+    // if it is wider than 480px, disable the listview toggle button,
+    // and make sure the list view showing
+    if (window.innerWidth > 480) {
+        self.enableListBtn(false);
+    }
+    $(window).load(function() {
+        $(window).resize(function() {
+            self.enableListBtn(window.innerWidth <= 480);
+            if (window.innerWidth > 480) {
+                self.toggleState(true);
+                $('#listContainer').slideDown();
+            }
+        });
     });
-
 }
 var viewModel = new ViewModel();
 
@@ -120,13 +117,13 @@ function initMap() {
     'use strict';
 
     // first set up the map
-    var map = new google.maps.Map(document.getElementById("map"), {
+    var map = new google.maps.Map(document.getElementById('map'), {
         center: {
-            "lat": 29.565943,
-            "lng": 106.547296
+            'lat': 29.565943,
+            'lng': 106.547296
         },
         scrollwheel: false,
-        zoom: 15
+        zoom: 10
     });
 
     //create one infowindow
@@ -136,21 +133,21 @@ function initMap() {
     function getWikipediaContent(marker, name) {
         // concerns of the ajax request, show an spinner GIF before
         // the real content is retrieved
-        infowindow.setContent("<img src='ajax-loader.gif'>");
+        infowindow.setContent('<img src="ajax-loader.gif">');
         infowindow.open(map, marker);
 
-        var wikiURL = "https://en.wikipedia.org/w/api.php?";
-        // these parameters: "prop" is to get wikipedia page's infomation
+        var wikiURL = 'https://en.wikipedia.org/w/api.php?';
+        // these parameters: 'prop' is to get wikipedia page's infomation
         // the page text and page's main image. exsentences is set to just 
         // get the first 6 sentences of the page.
         wikiURL += $.param({
-            "action": "query",
-            "titles": name,
-            "prop": "info|extracts|pageimages",
-            "inprop": "url",
-            "exsentences": 6,
-            "pithumbsize": 300,
-            "format": "json",
+            'action': 'query',
+            'titles': name,
+            'prop': 'info|extracts|pageimages',
+            'inprop': 'url',
+            'exsentences': 6,
+            'pithumbsize': 300,
+            'format': 'json',
         });
         $.ajax({
                 url: wikiURL,
@@ -159,22 +156,26 @@ function initMap() {
                     // open info window once get the infomations
                     var pages = e.query.pages[Object.keys(e.query.pages)[0]];
                     var pageLink = pages.canonicalurl;
-                    var pageImg = pages.thumbnail.source;
-                    // console.log(pages);
-                    infowindow.setContent("<div id='infoWindow'>" +
-                        "<img src=" + pageImg + ">" +
-                        pages.extract +
-                        "<a href=" + pageLink + ">" + "More on Wikipedia page" + "</a>" +
-                        "</div>");
+                    var pageImg = pages.thumbnail.source || 'http://placehold.it/300x200&text=Image+not+found';
 
-                    //set map center to selected marker
-                    // map.setCenter(marker.getPosition());
+                    infowindow.setContent('<div id="infoWindow">' +
+                        '<img src=' + pageImg + ' alt="location image">' +
+                        pages.extract +
+                        '<a href=' + pageLink + '>' + 'More on Wikipedia page' + '</a>' +
+                        '</div>');
+
+
                     //clear already selected icon
+
+                    // To reviewer, :)
+                    // markers is an object instead of an array, 
+                    // so, I use 'for in' to iterate it, I'm not sure if this
+                    // is ok? Can you tell me please. Thanks!
                     for (var key in markers) {
                         markers[key].setIcon(null);
                     }
                     // then change seleted marker icon to green
-                    marker.setIcon("http://maps.google.com/mapfiles/ms/icons/green-dot.png");
+                    marker.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
                 }
             })
             .fail(function() {
@@ -191,8 +192,11 @@ function initMap() {
         });
         marker.addListener('click', function(e) {
             // call third party API and set the infowindow content
-
             viewModel.selectedName(name);
+            //set map center to selected marker
+
+            map.panTo(marker.getPosition(), 3000);
+            // map.setZoom(14);
             getWikipediaContent(marker, name);
         });
         markers[name] = marker;
@@ -206,8 +210,17 @@ function initMap() {
         addMarker(map, latLng, data.name);
         mapBounds.extend(latLng);
     });
+    // fitbounds first time
     map.fitBounds(mapBounds);
+    // fitbounds everytime user resize the browser
+    $(window).resize(function() {
+        map.fitBounds(mapBounds);
+    });
 
     // binding view model
     ko.applyBindings(viewModel);
+}
+
+function mapErrorHandler() {
+    alert('Map can not be loaded...');
 }
